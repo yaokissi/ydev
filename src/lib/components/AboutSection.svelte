cd35838cea5427a6e8f99f48fabab17a8eca3f78
Kissi Yao <41991890+yaokissi@users.noreply.github.com>
2025-03-13 00:22:13 +0100

    feat: about section responsive

--- src/lib/components/AboutSection.svelte
+++ src/lib/components/AboutSection.svelte
@@ -1,102 +1,63 @@
-<script>
-    import { onMount } from "svelte";
+<script></script>
 
-    let hasCopied = false;
+<div id="about" class="w-full min-h-screen mx-auto bg-[#010103] p-10 grid gap-6
+    grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 auto-rows-auto">
 
-    function handleCopy() {
-        navigator.clipboard.writeText("yaokissi.pro@gmail.com");
-        hasCopied = true;
-
-        setTimeout(() => {
-            hasCopied = false;
-        }, 2000);
-    }
-</script>
-<section class="c-space my-20" id="about">
-    <div class="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
-        <div class="col-span-1 xl:row-span-3">
-            <div class="grid-container">
-                <img src="src/assets/grid1.png" alt="grid-1" class="w-full sm:h-[276px] h-fit object-contain" />
-
-                <div>
-                    <p class="grid-headtext">Hi, I’m Adrian Hajdin</p>
-                    <p class="grid-subtext">
-                        With 12 years of experience, I have honed my skills in both frontend and backend dev, creating dynamic
-                        and responsive websites.
-                    </p>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-span-1 xl:row-span-3">
-            <div class="grid-container">
-                <img src="src/assets/grid2.png" alt="grid-2" class="w-full sm:h-[276px] h-fit object-contain" />
-
-                <div>
-                    <p class="grid-headtext">Tech Stack</p>
-                    <p class="grid-subtext">
-                        I specialize in a variety of languages, frameworks, and tools that allow me to build robust and scalable
-                        applications
-                    </p>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-span-1 xl:row-span-4">
-            <div class="grid-container">
-                <div class="rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center">
-                    <img
-                            alt="globe"
-                            class="object-contain w-full h-[276px]"
-                            src="/src/assets/3d-Earth-Globe.svg"
-                    >
-                </div>
-                <div>
-                    <p class="grid-headtext">I’m very flexible with time zone communications & locations</p>
-                    <p class="grid-subtext">I&apos;m based in Rjieka, Croatia and open to remote work worldwide.</p>
-                    <span class="btn-ping_dot"></span>
-                    <button>Contact Me</button>
-                </div>
-            </div>
-        </div>
-
-        <div class="xl:col-span-2 xl:row-span-3">
-            <div class="grid-container">
-                <img src="src/assets/grid3.png" alt="grid-3" class="w-full sm:h-[266px] h-fit object-contain" />
-
-                <div>
-                    <p class="grid-headtext">My Passion for Coding</p>
-                    <p class="grid-subtext">
-                        I love solving problems and building things through code. Programming isn&apos;t just my
-                        profession—it&apos;s my passion. I enjoy exploring new technologies, and enhancing my skills.
-                    </p>
-                </div>
-            </div>
-        </div>
-
-        <div class="xl:col-span-1 xl:row-span-2">
-            <div class="grid-container">
-                <img
-                        src="src/assets/grid4.png"
-                        alt="grid-4"
-                        class="w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top"
-                />
-
-                <div class="xl:col-span-1 xl:row-span-2">
-                    <div class="grid-container">
-                        <img src="/src/assets/grid4.png" alt="grid-4" class="w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top" />
-                        <div class="space-y-2">
-                            <p class="grid-subtext text-center">Contact me</p>
-                            <div class=""  on:click={handleCopy}>
-                                <img src={hasCopied ? "/src/assets/tick.svg" : "/src/assets/copy.svg"} alt="copy mail" />
-                                <p class="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">yaokissi.pro@gmail.com</p>
-                            </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <!-- Card 1 (Prend 2 colonnes sur desktop) -->
+    <div class="bg-[#0e0e10] p-8 border border-gray-700 rounded-lg flex flex-col min-h-[350px]
+                lg:col-span-1">
+        <img src="/src/assets/grid1.png" alt="avatar" class="object-contain w-full h-[276px]" />
+        <h2 class="text-white font-bold mt-4">Hi, I'm Yao</h2>
+        <p class="text-white mt-5">
+            With 12 years of experience, I have honed my skills in both frontend and backend dev, creating dynamic and responsive websites.
+        </p>
     </div>
-</section>
+
+    <!-- Card 2 (Normal) -->
+    <div class="bg-[#0e0e10] border border-gray-700 p-8 rounded-lg flex flex-col min-h-[350px]">
+        <img src="/src/assets/grid2.png" alt="avatar" class="object-contain w-full h-[276px]" />
+        <h2 class="text-white font-bold mt-4">Tech Stack</h2>
+        <p class="text-white mt-5">
+            I specialize in a variety of languages, frameworks, and tools that allow me to build robust and scalable applications.
+        </p>
+    </div>
+
+    <!-- Card 3 (Prend 2 colonnes sur desktop) -->
+    <div class="bg-[#0e0e10] border border-gray-700 p-8 rounded-lg min-h-[350px]
+                lg:col-span-2">
+        <img src="/src/assets/grid3.png" alt="avatar" class="object-contain w-full h-[276px]" />
+        <h2 class="text-white font-bold mt-4">My Passion for Coding</h2>
+        <p class="text-white mt-5">
+            I love solving problems and building things through code. Programming isn't just my profession, it's my passion.
+            I enjoy exploring new technologies, and enhancing my skills.
+        </p>
+    </div>
+
+    <!-- Card 4 (Normal) -->
+    <div class="bg-[#0e0e10] border border-gray-700 p-8 rounded-lg min-h-[350px]">
+        <img alt="globe" class="object-contain w-full h-[276px]" src="/src/assets/3d-Earth-Globe.svg" />
+        <h2 class="text-white font-bold mt-4 text-center">
+            I'm very flexible with time zone communications & locations
+        </h2>
+        <p class="text-white mt-5 text-center">
+            I'm based in Rijeka, Croatia and open to remote work worldwide.
+        </p>
+        <button class="border border-gray-300 text-gray-800 px-5 py-2 rounded cursor w-full sm:w-auto hover:bg-gray-100 text-white mt-4">
+            <span class="btn-ping_dot"></span>
+            Contact me
+        </button>
+    </div>
+
+    <!-- Card 5 (Normal) -->
+    <div class="bg-[#0e0e10] border border-gray-700 p-8 rounded-lg min-h-[350px]">
+        <img src="/src/assets/grid4.png" alt="avatar" class="object-contain w-full h-[276px]" />
+        <h2 class="text-white text-center font-bold mt-2">Contact Me</h2>
+        <p class="text-white text-center">
+            <a href="mailto:yaokissi.pro@gmail.com" class="text-blue-400 hover:underline">yaokissi.pro@gmail.com</a>
+        </p>
+    </div>
+
+</div>
 <style>
     /*
     .rightSideAboutSection {
