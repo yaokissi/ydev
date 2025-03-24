d9b765ed90b5ba462b44d00e546b7694b2af26f2
Kissi Yao <41991890+yaokissi@users.noreply.github.com>
2025-03-24 23:09:31 +0100

    soroll x hidden

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@
 
 <svelte:head>
     <meta name="description" content="Yao KISSI portfolio, FrontEnd Developer From Paris passionate about building awesome websites" />
-    <meta name="keywords" content="yao kissi, kissi yao, développeur svelte junior ,développeur vuejs junior, Vuejs, Svelte, Tailwind, JavaScript" />
+    <meta name="keywords" content="yao kissi, kissi yao, développeur svelte junior,développeur vuejs junior, Vuejs, Svelte, Tailwind, JavaScript" />
     <meta name="author" content="Yao Kissi" />
     <meta name="google-site-verification" content="Kr3aB9ukxQAF3aNH2bBgRPKFw-6OdyK6ct8iXoyyxog" />
 </svelte:head>
