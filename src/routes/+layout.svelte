c520a0d90c664e1bfca82138807236dd3f5e2e86
Kissi Yao <41991890+yaokissi@users.noreply.github.com>
2025-03-24 20:52:34 +0100

    adding of google proprety validation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,7 @@
     <meta name="description" content="Yao KISSI portfolio, FrontEnd Developer From Paris passionate about building awesome websites" />
     <meta name="keywords" content="yao kissi, kissi yao, développeur svelte junior ,développeur vuejs junior, Vuejs, Svelte, Tailwind, JavaScript" />
     <meta name="author" content="Yao Kissi" />
+    <meta name="google-site-verification" content="Kr3aB9ukxQAF3aNH2bBgRPKFw-6OdyK6ct8iXoyyxog" />
 </svelte:head>
 
 <TopNavbar />
